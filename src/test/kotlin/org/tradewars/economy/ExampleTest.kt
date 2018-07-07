fa5d47ef3bb8fa46fb18385985b0db45c88428b4
Bios-Marcel <marceloschr@googlemail.com>
2018-07-07 17:42:16 +0200

    Add JUnit as dependency and add example test.

---
+++ src/test/kotlin/org/tradewars/economy/ExampleTest.kt
@@ -0,0 +1,21 @@
+package org.tradewars.economy
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+/**
+ * This class contains a simple example for writing a test with JUnit.
+ *
+ * @author Marcel
+ * @since 07.07.2018
+ */
+class ExampleTest {
+
+    /**
+     * This Test will run every time that you run the Gradle task `test`.
+     */
+    @Test
+    fun testExample() {
+        assertTrue("Hello JUnit".isNotEmpty(), "If this ever fails, something is clearly wrong.")
+    }
+}
